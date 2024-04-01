fix: corrections for efe computation
The commit applies a slight modification in the computation of the
B-novelty term. Also, it adds noise to the various probability
estimations to avoid divide by zero errors and related.

aifgym/agents/aif_agent.py
@@ -275,6 +275,8 @@ class ActInfAgent(BaifAgent):
 
         """
 
+        print("---------------------")
+        print("--- 1. PERCEPTION ---")
         # Retrieving the softmax function needed to normalise the updated values of the Q(S_t|pi)
         # after performing gradient descent.
         sigma = special.softmax
@@ -351,6 +353,10 @@ class ActInfAgent(BaifAgent):
             ########### 2. Update the Q(S_t|pi) by setting gradient to zero ##############
 
             for i in range(1):
+
+                # IMPORTANT: here we are replacing zero probabilities with the value 0.0001
+                # to avoid zeroes in logs.
+                self.Qs_pi = np.where(self.Qs_pi == 0, 0.0001, self.Qs_pi)
                 # Computing the variational free energy for the current policy
                 # Note 1: if B parameters are learned then you need to pass in self.B_params and
                 # self.learning_B (the same applies for A)
@@ -385,19 +391,27 @@ class ActInfAgent(BaifAgent):
                 )
 
                 # Simultaneous beliefs updates
+                # Note: the update equation below is based on the computations of Da Costa, 2020, p. 9,
+                # by setting the gradient to zero one can solve for the parameters that minimize that gradient,
+                # here we are recovering those solutions *from* the gradient (by subtraction) before applying
+                # a softmax to make sure we get valid probabilities.
                 # IMPORTANT: when using a mean-field approx. in variational inference (like it is commonly
                 # done in vanilla active inference) the various factors, e.g., the Q(S_t|pi), are updated
                 # one at time by keeping all the others fixed. Here, we are instead using a simultaneous
                 # update of all the factors, possibly repeating this operation a few times. However,
                 # results seem OK even if the for loop iterates just for one step.
+                # print(f"BEFORE update, Qs_pi: {self.Qs_pi[pi,:,-1]}")
+                # print(f"Gradient for update: {grad_F_pi}")
                 self.Qs_pi[pi, :, :] = sigma(
                     (-1) * (grad_F_pi - np.log(self.Qs_pi[pi, :, :]) - 1) - 1, axis=0
                 )
-
+                # print(f"AFTER update, Qs_pi: {self.Qs_pi[pi,:,-1]}")
+                # self.Qs_pi[pi, :, :] = sigma(-self.Qpi[pi, -1] * grad_F_pi, axis=0)
             ######### END ###########
 
             # Printing the free energy value for current policy at current time step
-            print(f"Policy {pi} - Step {self.current_tstep} - FE: {F_pi}")
+            print(f"Time Step: {self.current_tstep}")
+            print(f" FE_pi_{pi}: {F_pi}")
             # Storing the last computed free energy in self.free_energies
             self.free_energies[pi, self.current_tstep] = F_pi
             # Computing the policy-independent state probability at self.current_tstep and storing
@@ -441,6 +455,8 @@ class ActInfAgent(BaifAgent):
         - None.
 
         """
+        print("---------------------")
+        print("--- 2. PLANNING ---")
         # Retrieving the softmax function needed to normalise the values of vector Q(pi) after
         # updating them with the expected free energies.
         sigma = special.softmax
@@ -462,6 +478,14 @@ class ActInfAgent(BaifAgent):
             else:
                 # Note 1: if B parameters are learned then you need to pass in self.B_params
                 # and self.learning_B (the same applies for A)
+                ### DEBUGGING ###
+                # print(
+                #     f"The B params for action 2 (frist column): {self.B_params[2,:,0]}"
+                # )
+                # print(
+                #     f"The B params for action 2 (frist column): {self.B_params[2,:,3]}"
+                # )
+                ### END ###
                 G_pi, tot_Hs, tot_slog_s_over_C, tot_AsW_As, tot_AsW_Bs = efe(
                     self.num_states,
                     self.steps,
@@ -490,11 +514,31 @@ class ActInfAgent(BaifAgent):
                 self.efe_risk[pi, self.current_tstep] = tot_slog_s_over_C
                 self.efe_Anovelty[pi, self.current_tstep] = tot_AsW_As
                 self.efe_Bnovelty[pi, self.current_tstep] = tot_AsW_Bs
-                print(f"Policy {pi} - Step {self.current_tstep} - EFE: {G_pi}")
+                print(f"--- Summary of planning at time step {self.current_tstep} ---")
+                print(f"FE_{pi}: {F_pi}")
+                print(f"EFE_{pi}: {G_pi}")
+                print(f"Risk_{pi}: {tot_slog_s_over_C}")
+                print(f"Ambiguity {pi}: {tot_Hs}")
+                print(f"A-novelty {pi}: {tot_AsW_As}")
+                print(f"B-novelty {pi}: {tot_AsW_Bs}")
 
         # Normalising the negative expected free energies stored as column in self.Qpi to get
         # the posterior over policies Q(pi) to be used for action selection
+        print(f"Computing posterior over policy Q(pi)...")
         self.Qpi[:, self.current_tstep] = sigma(-self.Qpi[:, self.current_tstep])
+        print(f"Before adding noise - Q(pi): {self.Qpi}")
+        # Replacing zeroes with 0.0001, to avoid the creation of nan values and multiplying by 5 to make sure
+        # the concentration of probabilities is preserved when reapplying the softmax
+        self.Qpi[:, self.current_tstep] = np.where(
+            self.Qpi[:, self.current_tstep] == 1, 5, self.Qpi[:, self.current_tstep]
+        )
+        self.Qpi[:, self.current_tstep] = np.where(
+            self.Qpi[:, self.current_tstep] == 0,
+            0.0001,
+            self.Qpi[:, self.current_tstep],
+        )
+        self.Qpi[:, self.current_tstep] = sigma(self.Qpi[:, self.current_tstep])
+        print(f"After adding noise - Q(pi): {self.Qpi}")
         # Computing the policy-independent state probability at self.current_tstep and storing it in self.Qs
         self.Qs[:, self.current_tstep] += (
             self.Qs_pi[pi, :, self.current_tstep] * self.Qpi[pi, self.current_tstep]
@@ -645,9 +689,12 @@ class ActInfAgent(BaifAgent):
             - None.
         """
 
+        print("---------------------")
+        print("--- 4. LEARNING ---")
         # Getting the updated parameters for matrices A and B using dirichlet_update().
         # Note 1: if A or B parameters are *not* learned the update method simply return self.A_params or
         # self.B_params
+        print("Updating Dirichlet parameters...")
         self.A_params, self.B_params = dirichlet_update(
             self.num_states,
             self.num_actions,
@@ -671,6 +718,7 @@ class ActInfAgent(BaifAgent):
         # below (they do not change from their initialised form).
         if self.learning_A == True and self.learning_B == True:
 
+            print("Updated parameters for matrices A and Bs.")
             # After learning A's parameters, for every state draw one sample from the Dirichlet
             # distribution using the corresponding column of parameters.
             for s in range(self.num_states):
@@ -685,6 +733,7 @@ class ActInfAgent(BaifAgent):
 
         elif self.learning_A == True and self.learning_B == False:
 
+            print("Updated parameters for matrix A (no Bs learning).")
             # After learning A's parameters, for every state draw one sample from the Dirichlet
             # distribution using the corresponding column of parameters.
             for s in range(self.num_states):
@@ -692,6 +741,7 @@ class ActInfAgent(BaifAgent):
 
         elif self.learning_A == False and self.learning_B == True:
 
+            print("Updated parameters for matrices Bs (no A learning).")
             # After learning B's parameters, sample from them to update the B matrices, i.e. for every
             # action and state draw one sample from the Dirichlet distribution using the corresponding
             # column of parameters.
@@ -701,7 +751,7 @@ class ActInfAgent(BaifAgent):
 
         elif self.learning_A == False and self.learning_B == False:
 
-            pass
+            print("No update (matrices A and Bs not subject to learning).")
 
     def step(self, new_obs):
         """This method brings together all computational processes defined above, forming the
@@ -739,6 +789,8 @@ class ActInfAgent(BaifAgent):
 
             self.perception()
             self.planning()
+            print("---------------------")
+            print("--- 3. ACTING ---")
             self.current_action = eval(self.select_action)
             # Computing the total free energy and store it in self.total_free_energies
             # (as a reference for the agent performance)

aifgym/agents/utils_actinf.py
@@ -72,6 +72,8 @@ def vfe(
     #  # Fourth term: sum of dot products from t=2 to T.
     #  - S_2T(Q(s_t|pi) @ log(B_t-1) @ Q(s_t-1|pi)).
 
+    print("Computing FE...")
+
     # Digamma function used to compute the expectations of matrices A and B if their respective
     # parameters are learned by the agent. Those expectations turn up in the computation of the
     # free energy (see below).
@@ -84,11 +86,12 @@ def vfe(
     # need to be transposed because we want the dot products between the categorical distribution,
     # which are stored as columns in the original matrices.
 
-    # IMPORTANT: here we are replacing zero probabilities with the value 0.000001 to avoid zeroes in logs.
-    # Qs_pi_first = np.where(Qs_pi[pi, :, :] == 0, 0.0001, Qs_pi[pi, :, :])
+    # IMPORTANT: here we are replacing zero probabilities with the value 0.0001 to avoid zeroes in logs.
+    # Qs_pi = np.where(Qs_pi == 0, 0.0001, Qs_pi)
     # print(Qs_pi[pi, :, :])
     st_log_st = np.sum(np.sum(Qs_pi[pi, :, :].T * np.log(Qs_pi[pi, :, :]).T, axis=1))
 
+    # st_log_st = np.sum(np.sum(Qs_pi_first.T * np.log(Qs_pi_first).T, axis=1))
     ####################################### Second term #######################################
     # The second term, indicated by ot_logA_st, is computed in a vectorized way in the same way
     # as the first term.
@@ -216,6 +219,7 @@ def grad_vfe(
     as columns, one for each Q(s_t|pi).
     """
 
+    print("Computing FE gradients...")
     # Initialising the gradient vectors for each Q(s_t|pi)
     grad_F_pi = np.zeros((num_states, steps))
 
@@ -489,6 +493,7 @@ def efe(
     - G_pi (float): the sum of pi's expected free energies, one for each of future time step considered.
     """
 
+    print("Computing EFE...")
     # Initialising the expected free energy variable and its components
     G_pi = 0
     # Ambiguity
@@ -543,8 +548,8 @@ def efe(
                     )
                     # Computing the B-novelty term
                     AsW_Bs = np.dot(
-                        np.matmul(A, Qs_pi[pi, :, tau]),
-                        np.matmul(W_B, Qs_pi[pi, :, tau]),
+                        np.matmul(A, Qs_pi[pi, :, tau + 1]),
+                        np.matmul(W_B, Qs_pi[pi, :, tau + 1]),
                     )
                 # Computing the Ambiguity term
                 Hs = np.dot(H, Qs_pi[pi, :, tau])
@@ -636,7 +641,7 @@ def efe(
             # Making sure that tau is less than or equal to the total time steps in an episode;
             # we write (episode_steps-1) because we count from 0
             if tau <= (episode_steps - 1):
-                print(f"At time step tau: {tau}")
+                print(f"Computing EFE for future step {tau}...")
                 # Computing the terms of the expected free energy: ambiguity, risk, and novelty
                 # At the last state there is no action so when tau = episode_steps-1 the B-novelty term
                 # is dropped from the expected free energy.
@@ -656,10 +661,25 @@ def efe(
                             - 1 / np.sum(B_params[action, :, :], axis=0)
                         )
                     )
+
+                    #### DEBUGGING ####
+                    # b = 1 / B_params[action, :, :]
+                    # b_zero = 1 / np.sum(B_params[action, :, :], axis=0)
+                    # print(f"First row of b: {b[0,:]}")
+                    # print(f"First row of b_zero: {b_zero[0]}")
+                    # print(f"The Q(s|pi): {Qs_pi[pi, :, tau]}")
+                    # print(
+                    #     f"The A @ Q(s|pi) matrix for B-novelty: {np.matmul(A, Qs_pi[pi, :, tau])}"
+                    # )
+                    # print(
+                    #     f"The W_B @ Q(s|pi) matrix for B-novelty: {np.matmul(W_B, Qs_pi[pi, :, tau])}"
+                    # )
+                    #### END ####
+
                     # Computing B-novelty term
                     AsW_Bs = np.dot(
-                        np.matmul(A, Qs_pi[pi, :, tau]),
-                        np.matmul(W_B, Qs_pi[pi, :, tau]),
+                        np.matmul(A, Qs_pi[pi, :, tau + 1]),
+                        np.matmul(W_B, Qs_pi[pi, :, tau + 1]),
                     )
 
                 # IMPORTANT: here we are replacing zero probabilities with the minimum value in C to
@@ -679,6 +699,13 @@ def efe(
                     slog_s_over_C = np.dot(
                         Qs_pi_risk, np.log(Qs_pi_risk) - np.log(C[:, tau])
                     )
+                    ### DEBUGGING ###
+                    # print(f"Q(s|pi), corrected: {Qs_pi_risk}")  # ; Preferences: {C}")
+                    # print(
+                    #     f"logQ(S|pi) - log C: {np.log(Qs_pi_risk) - np.log(C[:, tau])}"
+                    # )
+                    # print(f"Risk: {slog_s_over_C}")
+                    ### END ###
 
                 else:
                     # Computing risk based on preferred observations
@@ -874,8 +901,10 @@ def dirichlet_update(
         # Getting the approximate posterior
         Q_A_params = A_params  # Nothing is learned here
         # assert np.array_equal(Dirichlet_update_B[2,:,:], Dirichlet_update_B[3,:,:]) == False, 'Updates suspiciously identical!'
+        print(f"Old B params {B_params[2,:]}")
+        # print(f"Dirichlet update: {Dirichlet_update_B}")
         Q_B_params = B_params + Dirichlet_update_B
-
+        print(f"New B params {B_params[2,:]}")
         # Returning the approximate posterior for the learned parameters (Q_A_params is equal to
         # A_params because A is not learned)
         return Q_A_params, Q_B_params

aifgym/visuals/utils_vis.py
@@ -50,6 +50,9 @@ def plot_pi_fe(
 
         pi_runs = data["pi_probabilities"][:, -1, select_policy, -1]
         selected_runs = (pi_runs > 0.5).nonzero()[0]
+        print(
+            f"Number of runs with policy {select_policy} more probable: {len(selected_runs)}/{len(pi_runs)}"
+        )
         pi_fe = data["pi_free_energies"][selected_runs]
     else:
         pi_fe = data["pi_free_energies"]
@@ -101,7 +104,7 @@ def plot_pi_fe(
         ax.set_xlabel("Episode")
         # ax.set_ylabel('Free Energy', rotation=90)
         ax.legend(loc="upper right")
-        ax.set_title("Free Energy at the Last Step\n")
+        ax.set_title("Last Step Free Energy\n")
         ax.fill_between(
             x2,
             y2 - (1.96 * std_pi_fe[:, -1] / np.sqrt(num_runs)),
@@ -163,9 +166,9 @@ def plot_pi_fe_compare(
         step_fe_pi >= 0 and step_fe_pi <= num_steps - 1
     ) or step_fe_pi == -1, "Invalid step number."
 
-    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig, ax = plt.subplots()
 
-    # Looping over the policies
+    # Looping over the policies for Figure 1
     for p in range(num_policies):
 
         # Computing the mean (average) and std of one policy's free energies over the runs
@@ -181,39 +184,69 @@ def plot_pi_fe_compare(
         x1 = np.arange(num_episodes * num_steps)
         y1 = avg_pi_fe.flatten()
 
-        ax1.plot(x1, y1, ".-", label=f"Policy $\\pi_{p}$")
-        ax1.set_xticks(np.arange(0, (num_episodes * num_steps) + 1, step=x_ticks_tstep))
-        ax1.set_xlabel("Step")
-        # ax1.ylabel('Free Energy', rotation=90)
-        ax1.legend(loc="upper right")
-        ax1.set_title("Free Energy at Every Step\n")
+        ax.plot(x1, y1, ".-", label=f"Policy $\\pi_{p}$")
+
+    # Completing drawing axes for Figure 1
+    ax.set_xticks(np.arange(0, (num_episodes * num_steps) + 1, step=x_ticks_tstep))
+    ax.set_xlabel("Step")
+    # ax1.ylabel('Free Energy', rotation=90)
+    ax.legend(loc="upper right")
+    ax.set_title("Free Energy at Every Step\n")
+
+    # Save figures and show
+    fig.savefig(
+        save_dir + "/" + f"pi_fes_compare_every_step.jpg",
+        format="jpg",
+        bbox_inches="tight",
+        pad_inches=0.1,
+    )
+
+    plt.show()
+    # plt.close()
+
+    fig, ax = plt.subplots()
+    # Looping over the policies for Figure 2
+    for p in range(num_policies):
+
+        # Computing the mean (average) and std of one policy's free energies over the runs
+        # TODO: handle rare case in which you train only for one episode, in that case squeeze()
+        # will raise the exception
+        avg_pi_fe = np.mean(pi_fe[:, :, p, :], axis=0).squeeze()
+        std_pi_fe = np.std(pi_fe[:, :, p, :], axis=0).squeeze()
+        # Making sure avg_pi_fe has the right dimensions
+        # print(avg_pi_fe.shape)
+        # print((num_episodes, num_steps))
+        assert avg_pi_fe.shape == (num_episodes, num_steps), "Wrong dimenions!"
 
         # Plotting the free energy at the last time step of every episode for all episodes
         # Note 1: another time step can be chosen by changing the index number, i, in avg_pi_fe[:, i]
         x2 = np.arange(num_episodes)
         y2 = avg_pi_fe[:, step_fe_pi]
 
-        ax2.plot(x2, y2, ".-", label=f"Policy $\\pi_{p}$")
-        ax2.set_xticks(np.arange(0, num_episodes + 1, step=x_ticks_estep))
-        ax2.set_xlabel("Episode")
-        # ax2.set_ylabel('Free Energy', rotation=90)
-        ax2.legend(loc="upper right")
-        ax2.set_title("Free Energy at the Last Step\n")
-        ax2.fill_between(
-            x2,
-            y2 - (1.96 * std_pi_fe[:, -1] / np.sqrt(num_runs)),
-            y2 + (1.96 * std_pi_fe[:, -1] / np.sqrt(num_runs)),
-            alpha=0.3,
-        )
+        ax.plot(x2, y2, ".-", label=f"Policy $\\pi_{p}$")
 
-    # Save figure and show
-    plt.savefig(
-        save_dir + "/" + f"pi_fes_compare.jpg",
+    # Completing drawing axes for Figure 2
+    ax.set_xticks(np.arange(0, num_episodes + 1, step=x_ticks_estep))
+    ax.set_xlabel("Episode")
+    # ax2.set_ylabel('Free Energy', rotation=90)
+    ax.legend(loc="upper right")
+    ax.set_title("Last Step Free Energy\n")
+    ax.fill_between(
+        x2,
+        y2 - (1.96 * std_pi_fe[:, -1] / np.sqrt(num_runs)),
+        y2 + (1.96 * std_pi_fe[:, -1] / np.sqrt(num_runs)),
+        alpha=0.3,
+    )
+
+    fig.savefig(
+        save_dir + "/" + f"pi_fes_compare_last_step.jpg",
         format="jpg",
         bbox_inches="tight",
         pad_inches=0.1,
     )
+
     plt.show()
+    # plt.close()
 
 
 def plot_total_fe(
@@ -370,6 +403,80 @@ def plot_pi_prob(file_data_path, x_ticks_tstep, select_policy, save_dir):
     plt.show()
 
 
+def plot_pi_prob_last(file_data_path, x_ticks_tstep, select_policy, save_dir):
+    """Plotting the probability over policies, Q(pi), averaged over the runs at the last time step of each
+    episode during the experiment.
+
+    Inputs:
+
+    - file_data_path (string): file path where the data was stored (i.e. where log_data was saved);
+    - x_ticks_tstep (integer): step for the ticks in the x axis when plotting as a function of total timesteps;
+    - save_dir (string): directory where to save the images.
+
+    Outputs:
+
+    - plot showing the evolution of Q(pi) as the agent goes through the episodes.
+
+    Note 1: the probability mass should concentrate on the most favoured policy as the agent experiences
+    the maze (episode after episode).
+    """
+
+    # Retrieving the data dictionary and extracting the content of required keys, e.g. 'pi_probabilities'
+    data = np.load(file_data_path, allow_pickle=True).item()
+    num_runs = data["num_runs"]
+    num_episodes = data["num_episodes"]
+    num_policies = data["num_policies"]
+
+    # Ignoring certain runs depending on the final probability of a certain policy, if corresponding argument
+    # was passed through the command line
+    if select_policy != -1:
+
+        pi_runs = data["pi_probabilities"][:, -1, select_policy, -1]
+        selected_runs = (pi_runs > 0.5).nonzero()[0]
+        pi_prob = data["pi_probabilities"][selected_runs]
+    else:
+        pi_prob = data["pi_probabilities"]
+
+    # Averaging the policies' probabilities over the runs for last step of each episode only
+    avg_pi_prob = np.mean(pi_prob[:, :, :, -1], axis=0).squeeze()
+    std_pi_prob = np.std(pi_prob[:, :, :, -1], axis=0).squeeze()
+    # Making sure avg_pi_prob_ls has the right dimensions
+    assert avg_pi_prob.shape == (num_episodes, num_policies), "Wrong dimenions!"
+    # assert np.all(np.sum(avg_pi_prob_ls, axis=1)) == True, 'Probabilities do not sum to one!'
+
+    plt.figure()
+    x = np.arange(num_episodes)
+
+    for p in range(num_policies):
+        y = avg_pi_prob[:, p].flatten()
+        std = std_pi_prob[:, p].flatten()
+        plt.plot(x, y, ".-", label=f"Policy $\\pi_{p}$")
+
+        plt.fill_between(
+            x,
+            y - (1.96 * std / np.sqrt(num_runs)),
+            y + (1.96 * std / np.sqrt(num_runs)),
+            alpha=0.3,
+        )
+
+    plt.xticks(np.arange(0, num_episodes + 1, step=x_ticks_tstep))
+    plt.xlabel("Episode")
+    plt.ylabel("Probability Mass", rotation=90)
+    # plt.legend(['Policy 1', 'Policy 2', 'Policy 3', 'Policy 4', 'Policy 5'], loc='upper right')
+    plt.legend(loc="upper right")
+    plt.ticklabel_format(style="plain")
+    plt.ticklabel_format(useOffset=False)
+    plt.title("Probability over Policies\n")
+    # Save figure and show
+    plt.savefig(
+        save_dir + "/" + f"pi_probs_last.jpg",
+        format="jpg",
+        bbox_inches="tight",
+        pad_inches=0.1,
+    )
+    plt.show()
+
+
 def plot_efe(file_data_path, select_policy, save_dir):
     """Plotting the expected free energy, EFE, for a given policy over all the steps averaged over the runs.
 
@@ -427,7 +534,7 @@ def plot_efe(file_data_path, select_policy, save_dir):
     plt.title("Expected Free Energy at Every Step\n")
     # Save figure and show
     plt.savefig(
-        save_dir + "/" + "efe.pdf", format="jpg", bbox_inches="tight", pad_inches=0.1
+        save_dir + "/" + "efe.jpg", format="jpg", bbox_inches="tight", pad_inches=0.1
     )
     plt.show()
 
@@ -449,6 +556,7 @@ def plot_efe_comps(file_data_path, select_policy, save_dir):
 
     # Retrieving the data dictionary and extracting the content of required keys, e.g. 'total_free_energies'
     data = np.load(file_data_path, allow_pickle=True).item()
+    num_runs = data["num_runs"]
     num_episodes = data["num_episodes"]
     num_policies = data["num_policies"]
     num_steps = data["num_steps"]
@@ -459,6 +567,7 @@ def plot_efe_comps(file_data_path, select_policy, save_dir):
 
         pi_runs = data["pi_probabilities"][:, -1, select_policy, -1]
         selected_runs = (pi_runs > 0.5).nonzero()[0]
+        num_runs = len(selected_runs)
         efe = data["expected_free_energies"][selected_runs]
         efe_ambiguity = data["efe_ambiguity"][selected_runs]
         efe_risk = data["efe_risk"][selected_runs]
@@ -474,9 +583,13 @@ def plot_efe_comps(file_data_path, select_policy, save_dir):
     # Averaging the expected free energies and their components over the runs
     avg_efe = np.mean(efe, axis=0).squeeze()
     avg_efe_ambiguity = np.mean(efe_ambiguity, axis=0).squeeze()
+    std_efe_ambiguity = np.std(efe_ambiguity, axis=0).squeeze()
     avg_efe_risk = np.mean(efe_risk, axis=0).squeeze()
+    std_efe_risk = np.std(efe_risk, axis=0).squeeze()
     avg_efe_Anovelty = np.mean(efe_Anovelty, axis=0).squeeze()
+    std_efe_Anovelty = np.std(efe_Anovelty, axis=0).squeeze()
     avg_efe_Bnovelty = np.mean(efe_Bnovelty, axis=0).squeeze()
+    std_efe_Bnovelty = np.std(efe_Bnovelty, axis=0).squeeze()
     # Making sure efe has the right dimensions
     assert avg_efe.shape == (num_episodes, num_policies, num_steps), "Wrong dimenions!"
     assert avg_efe_ambiguity.shape == (
@@ -512,17 +625,47 @@ def plot_efe_comps(file_data_path, select_policy, save_dir):
         x = np.arange(num_episodes)
         # x = np.arange(1*(num_steps-1))
         y_efea = avg_efe_ambiguity[:, p, 1].flatten()
+        stdy_efea = std_efe_ambiguity[:, p, 1].flatten()
         # print(y_efea.shape)
         # print(y_efea[0:10])
         y_efer = avg_efe_risk[:, p, 1].flatten()
+        stdy_efer = std_efe_risk[:, p, 1].flatten()
         y_efeA = avg_efe_Anovelty[:, p, 1].flatten()
+        stdy_efeA = std_efe_Anovelty[:, p, 1].flatten()
         y_efeB = avg_efe_Bnovelty[:, p, 1].flatten()
+        stdy_efeB = std_efe_Bnovelty[:, p, 1].flatten()
         # y = np.reshape(-avg_efe[2, p, 0:-1], (1*(num_steps-1)))
 
         plt.plot(x, y_efea, ".-", label=f"Ambiguity for $\\pi_{p}$")
         plt.plot(x, y_efer, ".-", label=f"Risk for $\\pi_{p}$")
         plt.plot(x, y_efeA, ".-", label=f"A-novelty for $\\pi_{p}$")
         plt.plot(x, y_efeB, ".-", label=f"B-novelty for $\\pi_{p}$")
+
+        plt.fill_between(
+            x,
+            y_efea - (1.96 * stdy_efea / np.sqrt(num_runs)),
+            y_efea + (1.96 * stdy_efea / np.sqrt(num_runs)),
+            alpha=0.3,
+        )
+        plt.fill_between(
+            x,
+            y_efer - (1.96 * stdy_efer / np.sqrt(num_runs)),
+            y_efer + (1.96 * stdy_efer / np.sqrt(num_runs)),
+            alpha=0.3,
+        )
+
+        plt.fill_between(
+            x,
+            y_efeA - (1.96 * stdy_efeA / np.sqrt(num_runs)),
+            y_efeA + (1.96 * stdy_efeA / np.sqrt(num_runs)),
+            alpha=0.3,
+        )
+        plt.fill_between(
+            x,
+            y_efeB - (1.96 * stdy_efeB / np.sqrt(num_runs)),
+            y_efeB + (1.96 * stdy_efeB / np.sqrt(num_runs)),
+            alpha=0.3,
+        )
 
     plt.xlabel("Step")
     plt.ylabel("Value", rotation=90)