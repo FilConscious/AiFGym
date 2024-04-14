fix: correct computation of B term in EFE
The commit amends the computation of the KL divergence for the
parameters of B matrices in the expected free energy, i.e., using the
right time steps (t and t+1).

aifgym/agents/utils_actinf.py
@@ -682,7 +682,7 @@ def efe(
                     # Computing B-novelty term
                     AsW_Bs = np.dot(
                         np.matmul(A, Qs_pi[pi, :, tau + 1]),
-                        np.matmul(W_B, Qs_pi[pi, :, tau + 1]),
+                        np.matmul(W_B, Qs_pi[pi, :, tau]),
                     )
                     if current_tstep == 0:
                         print(f"EFE at future time step {tau}: {AsW_Bs}")